ed776d68fdde95f48f28405145d0f2996e40cf27
Lőrinc Serfőző <serfozo.lorinc@gmail.com>
2018-02-19 19:38:45 +0200

    added data dump

---
+++ db/chessApi_data_dump.sql
@@ -0,0 +1,8 @@
+INSERT INTO "users" VALUES(1,'Mystery',1362015937,'mystery@mymail.com');
+INSERT INTO "users" VALUES(2,'AxelW',1357724086,'axelw@mymail.com');
+INSERT INTO "users" VALUES(3,'LinuxPenguin',1362012937,'linuxpenguin@mymail.com');
+INSERT INTO "users" VALUES(4,'Koodari',1389260086,'koodari@mymail.com');
+INSERT INTO "users" VALUES(5,'HockeyFan',1394357686,'hockeyfan@mymail.com');
+INSERT INTO "exercises" VALUES(1,1,'Easy one','No need to explain this.', 1519061565, 'VALIDFENFEN','VALIDPGNPGN');
+INSERT INTO "exercises" VALUES(2,1,'Easier one','No need to explain this.', 1443827483, 'VALIDFENFEN','VALIDPGNPGN');
+INSERT INTO "exercises" VALUES(3,4,'Tough','Move the rook.', 1502337273, 'VALIDFENFEN','VALIDPGNPGN');
