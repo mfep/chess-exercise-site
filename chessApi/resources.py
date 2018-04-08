78c10ed1db0d09bcbebf61f2efbce32c7c885af1
Lőrinc Serfőző <serfozo.lorinc@gmail.com>
2018-04-08 16:30:38 +0300

    added error objects

--- chessApi/resources.py
+++ chessApi/resources.py
@@ -4,10 +4,15 @@ Defines the REST resources used by the API.
 @author: lorinc
 
 """
-
-from flask import Flask
+import json
+from flask import Flask, request, Response, g, _request_ctx_stack
 from flask_restful import Resource, Api
 from chessApi import database
+
+MASON = 'application/vnd.mason+json'
+JSON = 'application/json'
+ERROR_PROFILE = '/profiles/error-profile'
+LINK_RELATIONS = '/api/link-relations/'
 
 app = Flask(__name__)
 app.debug = True
@@ -15,9 +20,81 @@ app.config.update({'Engine': database.Engine()})
 api = Api(app)
 
 
-class MasonObject(dict):
+class ChessApiObject(dict):
     def __init__(self, **kwargs):
-        super(MasonObject, self).__init__(**kwargs)
+        super(ChessApiObject, self).__init__(**kwargs)
+        self['@namespaces'] = {
+            'chessapi': {
+                'name': LINK_RELATIONS
+            }
+        }
+
+
+def create_error_response(status_code, title, message=None):
+    """
+    Creates a: py: class:`flask.Response` instance when sending back an
+    HTTP error response
+
+    : param integer status_code: The HTTP status code of the response
+    : param str title: A short description of the problem
+    : param message: A long description of the problem
+    : rtype:: py: class:`flask.Response`
+    """
+
+    resource_url = None
+    ctx = _request_ctx_stack.top
+    if ctx is not None:
+        resource_url = request.path
+    envelope = {
+        'resource_url': resource_url,
+        '@error': {
+            '@message': title,
+            '@messages': [message]
+        }
+    }
+    return Response(json.dumps(envelope), status_code, mimetype=MASON+';'+ERROR_PROFILE)
+
+
+@app.errorhandler(400)
+def resource_not_found(error):
+    return create_error_response(400, "Malformed input format",
+                                 "The format of the input is incorrect")
+
+
+@app.errorhandler(404)
+def resource_not_found(error):
+    return create_error_response(404, "Resource not found",
+                                 "This resource url does not exit")
+
+
+@app.errorhandler(500)
+def unknown_error(error):
+    return create_error_response(500, "Internal error",
+                                 "The system has failed. Please, contact the administrator")
+
+
+@app.before_request
+def connect_db():
+    """
+    Creates a database connection before the request is proccessed.
+
+    The connection is stored in the application context variable flask.g .
+    Hence it is accessible from the request object.
+    """
+
+    g.con = app.config["Engine"].connect()
+
+
+@app.teardown_request
+def close_connection(exc):
+    """
+    Closes the database connection
+    Check if the connection is created. It migth be exception appear before
+    the connection is created.
+    """
+
+    if hasattr(g, "con"):
+        g.con.close()
 
 
 class Users(Resource):
