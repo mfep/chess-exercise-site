6569b7716af9847c078225067188ba815684ad14
Lőrinc Serfőző <serfozo.lorinc@gmail.com>
2018-05-05 13:57:53 +0300

    board numbers

--- chessApi_site/static/js/chess_board.js
+++ chessApi_site/static/js/chess_board.js
@@ -30,8 +30,8 @@ var ChessBoard = function() {
     }
 
     this.drawBoard = function () {
-        svgnode.setAttributeNS(null, "width", (8 * TILE).toString());
-        svgnode.setAttributeNS(null, "height", (8 * TILE).toString());
+        svgnode.setAttributeNS(null, "width", (9 * TILE).toString());
+        svgnode.setAttributeNS(null, "height", (9 * TILE).toString());
 
         function addRect(x, y, w, h, color) {
             var groupElem = document.createElementNS(xmlns, "g");
@@ -46,16 +46,37 @@ var ChessBoard = function() {
             return groupElem;
         }
 
+        function addTextRect(x, y, text) {
+            var textElem = document.createElementNS(xmlns, "text");
+            textElem.setAttributeNS(null, "x", x);
+            textElem.setAttributeNS(null, "y", y);
+            textElem.setAttributeNS(null, "text-anchor", "middle");
+            var textNode = document.createTextNode(text);
+            textElem.appendChild(textNode);
+            svgnode.appendChild(textElem);
+        }
+
         // tiles
         for (var y = 0; y < 8; y++) {
             var row = [];
             for (var x = 0; x < 8; x++) {
                 var color = (9 * y + x) % 2 ? "#af6336" : "#fcfa9f";
-                var rectGroup = addRect(x * TILE, y * TILE, TILE, TILE, color);
+                var rectGroup = addRect(x * TILE + TILE, y * TILE + TILE, TILE, TILE, color);
                 rectGroup.onclick = createRectGroupClicked(x, y);
                 row.push(rectGroup);
             }
             tiles.push(row);
+        }
+
+        // indicator letters
+        for (x = 0; x < 8; x++) {
+            var letter = String.fromCharCode('a'.charCodeAt(0) + x);
+            addTextRect((x+1.5) * TILE, TILE / 2, letter);
+        }
+
+        // indicator indices
+        for (y = 0; y < 8; y++) {
+            addTextRect(TILE / 2, (y+1.5)*TILE, (8-y).toString());
         }
 
         // highlighter
